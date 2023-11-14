2fd0dde450599dd837d74849ee05243180fbb9c3
artag711 <artagg711@gmail.com>
2023-11-14 19:50:40 +0300

    aasohbet

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const socket = require("socket.io")
+
+
+const app = express()
+const server = app.listen(8080, () => {
+    console.log("server");
+});
+
+app.use(express.static("public"))
+
+
+const io = socket(server)
+
+
+io.on("connection", (socket) => {
+    console.log(socket.id)
+
+
+    socket.on("chat", data =>{
+        io.sockets.emit("chat", data)
+
+    })
+    
+    socket.on("typing", data => {
+        socket.broadcast.emit("typing", data)
+
+    })
+})
